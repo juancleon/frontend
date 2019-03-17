c0b53c59669f58916d6d7671c035eb0f955637fc
juancleon <31444927+juancleon@users.noreply.github.com>
2019-03-16 18:54:56 -0700

    Add files via upload

---
+++ src/components/delete-application.component.js
@@ -0,0 +1,60 @@
+import React, {Component } from 'react';
+import axios from 'axios';
+
+export default class DeleteApplication extends Component {
+  constructor(props) {
+      super(props);
+
+      this.delete = this.delete.bind(this);
+
+      this.state = {
+          school: '',
+          status: ''
+      }
+  }
+
+  componentDidMount(){
+      axios.get('http://localhost:4000/project/applications/'+this.props.match.params.id)
+          .then(response => {
+              this.setState({
+                  school: response.data.school,
+                  status: response.data.status
+              })
+          })
+          .catch(function(error){
+              console.log(error)
+          })
+  }
+
+  delete(e) {
+      e.preventDefault();
+      axios.delete('http://localhost:4000/project/applications/delete/' + this.props.match.params.id)
+          .then(res => console.log(res.data));
+      this.props.history.push('/applications');
+  }
+
+  render() {
+      return (
+          <div>
+              <h3>Are you sure you want to delete this application?</h3>
+                <table className="table table-striped" style={{marginTop: 20}}>
+                <thead>
+                    <tr>
+                        <th>School</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                  <tbody>
+                    <tr>
+                      <td>{this.state.school} </td>
+                      <td>{this.state.status} </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <form onSubmit={this.delete}>
+                      <input type="submit" value="Delete Application" className="btn btn-primary" />
+                </form>
+          </div>
+      )
+  }
+}
---
+++ src/components/delete-testscore.component.js
@@ -0,0 +1,64 @@
+import React, {Component } from 'react';
+import axios from 'axios';
+
+export default class DeleteTestScore extends Component {
+  constructor(props) {
+      super(props);
+
+      this.delete = this.delete.bind(this);
+
+      this.state = {
+          testType: '',
+          mathScore: '',
+          verbalScore: ''
+      }
+  }
+
+  componentDidMount(){
+      axios.get('http://localhost:4000/project/testScores/'+this.props.match.params.id)
+          .then(response => {
+              this.setState({
+                testType: response.data.testType,
+                mathScore: response.data.mathScore,
+                verbalScore: response.data.verbalScore,
+              })
+          })
+          .catch(function(error){
+              console.log(error)
+          })
+  }
+
+  delete(e) {
+      e.preventDefault();
+      axios.delete('http://localhost:4000/project/testScores/delete/' + this.props.match.params.id)
+          .then(res => console.log(res.data));
+      this.props.history.push('/testScores');
+  }
+
+  render() {
+      return (
+          <div>
+              <h3>Are you sure you want to delete this test score?</h3>
+                <table className="table table-striped" style={{marginTop: 20}}>
+                <thead>
+                    <tr>
+                        <th>Test Type</th>
+                        <th>Math Score</th>
+                        <th>Verbal Score</th>
+                    </tr>
+                </thead>
+                  <tbody>
+                    <tr>
+                      <td>{this.state.testType} </td>
+                      <td>{this.state.mathScore} </td>
+                      <td>{this.state.verbalScore} </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <form onSubmit={this.delete}>
+                      <input type="submit" value="Delete Test Score" className="btn btn-primary" />
+                </form>
+          </div>
+      )
+  }
+}
