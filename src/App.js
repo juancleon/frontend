7f0d110384dfb1abf95e11fe99a36f529020b80c
juancleon <31444927+juancleon@users.noreply.github.com>
2019-03-16 18:58:41 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";//npm install bootstrap
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";//npm install react-router-dom
 import Applications from "./components/applications.component";
 import CreateApplication from "./components/create-application.component";
-import CreateTestScore from "./components/create-testscore.component";
 import EditApplication from "./components/edit-application.component";
+import DeleteApplication from "./components/delete-application.component";
+import TestScores from "./components/testscores.component";
+import CreateTestScore from "./components/create-testscore.component";
 import EditTestScore from "./components/edit-testscore.component";
+import DeleteTestScore from "./components/delete-testscore.component";
 import SchoolRecommenderSearchResults from "./components/school-recommender-search-results.component";
 import SchoolRecommender from "./components/school-recommender.component";
-import TestScores from "./components/testscores.component";
+
 //npm install axios
 
 {/*import CreateTodo from "./components/create-todo.component";
@@ -55,11 +58,13 @@ class App extends Component {
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />*/}
           <Route path="/applications" component={Applications} />
-          <Route path="/editApplication/:id" component={EditApplication} />
           <Route path="/createApplication" component={CreateApplication} />
+          <Route path="/editApplication/:id" component={EditApplication} />
+          <Route path="/deleteApplication/:id" component={DeleteApplication} />
           <Route path="/testScores" component={TestScores} />
-          <Route path="/editTestScore/:id" component={EditTestScore} />
           <Route path="/createTestScore" component={CreateTestScore} />
+          <Route path="/editTestScore/:id" component={EditTestScore} />
+          <Route path="/deleteTestScore/:id" component={DeleteTestScore} />
           <Route path="/" exact component={SchoolRecommender} />
           <Route path="/searchResults" component={SchoolRecommenderSearchResults} />
         </div>
