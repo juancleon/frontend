e2ff5691d782e3040d62ec766c15c60cd262b664
juancleon <31444927+juancleon@users.noreply.github.com>
2019-05-08 19:04:16 -0700

    Add files via upload

--- src/components/testscores.component.js
+++ src/components/testscores.component.js
@@ -26,8 +26,14 @@ const TestScore = props => (
 export default class TestScores extends Component {
   constructor(props) {
       super (props);
+
+      this.onChangeSortCriteria = this.onChangeSortCriteria.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+
       this.state = {testScores: [],
-        _isMounted: false}
+        _isMounted: false,
+        sortCriteria: '',
+      }
   }
 
   componentDidMount() {
@@ -63,12 +69,91 @@ export default class TestScores extends Component {
       });
   }
 
+  onChangeSortCriteria(e){
+      this.setState({
+          sortCriteria: e.target.value
+      });
+  }
+
+  onSubmit(e) {
+      e.preventDefault();
+      //console.log(`Is mounted inside submit: ${this.state._isMounted}`);
+      //console.log(`Is mounted inside submit: ${this._isMounted}`);
+      this._isMounted = false;
+      /*this.setState(prevState => ({
+          _callComponentDidUpdate: !prevState._callComponentDidUpdate
+      }));*/
+
+      axios.get('http://localhost:4000/sortTestScores/' + this.state.sortCriteria)
+        .then(res => {
+
+            this.setState({testScores: res.data});
+            /*console.log(`Sort Criteria: ${this.state.sortCriteria}`);
+            console.log(`Call Component Did Update: ${this.state._callComponentDidUpdate}`);*/
+
+        })
+        .catch( error => {
+          console.log(error);
+        });
+}
+
   render() {
       return (
             <div>
             {<NavigationBar/>}
               <div>
               <h3> Test Scores List</h3>
+              <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                      <input type="submit" value="Sort" className="btn btn-primary" />
+                  {/*</div>
+                  <div className="form-group">*/}
+                      <div className="form-check form-check-inline">
+                              <input className="form-check-input"
+                                     type="radio"
+                                     name="sortOptions"
+                                     id="sortByTestType"
+                                     value="testType"
+                                     checked={this.state.sortCriteria==='testType'}
+                                     onChange={this.onChangeSortCriteria}
+                                     />
+                              <label className="form-check-label">Test Type</label>
+                        </div>
+                          <div className="form-check form-check-inline">
+                              <input className="form-check-input"
+                                     type="radio"
+                                     name="sortOptions"
+                                     id="sortByMathScore"
+                                     value="mathScore"
+                                     checked={this.state.sortCriteria==='mathScore'}
+                                     onChange={this.onChangeSortCriteria}
+                                     />
+                              <label className="form-check-label">Math Score</label>
+                          </div>
+                          <div className="form-check form-check-inline">
+                              <input className="form-check-input"
+                                     type="radio"
+                                     name="sortOptions"
+                                     id="sortByverbalScore"
+                                     value="verbalScore"
+                                     checked={this.state.sortCriteria==='verbalScore'}
+                                     onChange={this.onChangeSortCriteria}
+                                     />
+                              <label className="form-check-label">Verbal Score</label>
+                          </div>
+                          <div className="form-check form-check-inline">
+                              <input className="form-check-input"
+                                     type="radio"
+                                     name="sortOptions"
+                                     id="sortByDateTaken"
+                                     value="dateTaken"
+                                     checked={this.state.sortCriteria==='dateTaken'}
+                                     onChange={this.onChangeSortCriteria}
+                                     />
+                              <label className="form-check-label">Date Taken</label>
+                          </div>
+                    </div>
+              </form>
               <table className="table table-striped" style={{marginTop: 20}}>
                   <thead>
                       <tr>
